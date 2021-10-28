37791697993cfe5459ce45d8aed6419e999967ab
Nikita Aleshchenko <n.aleshchenko@tinkoff.ru>
2021-10-28 18:04:40 +0300

    Small fixes

--- src/main/scala/movies/part2/MovieField.scala
+++ src/main/scala/movies/part2/MovieField.scala
@@ -1,7 +1,7 @@
 package movies.part2
 
-import movies.part1.Order._
-import scala.Ordering._
+import movies.part1.Order
+import java.time.LocalDate
 
 sealed trait MovieField {
   def withOrder(order: Order): Ordering[Movie]
@@ -10,14 +10,14 @@ sealed trait MovieField {
 object MovieField {
   case object Name extends MovieField {
     override def withOrder(order: Order): Ordering[Movie] =
-      order(Ordering.Int).on(_.name)
+      order(Ordering[String]).on(_.name)
   }
   case object ReleaseDate extends MovieField {
     override def withOrder(order: Order): Ordering[Movie] =
-      order(Ordering.LocalDate).on(_.releaseDate)
+      order(Ordering[LocalDate]).on(_.releaseDate)
   }
   case object Rating extends MovieField {
     override def withOrder(order: Order): Ordering[Movie] =
-      order(Ordering.LocalDate).on(_.releaseDate)
+      order(Ordering[Int]).on(_.rating)
   }
 }
