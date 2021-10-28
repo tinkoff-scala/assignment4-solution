bfc7e6a20b4079d4b2a7b81e0e166d4b42d1c63a
Nikita Aleshchenko <n.aleshchenko@tinkoff.ru>
2021-10-28 17:13:56 +0300

    Basic ordering

--- src/main/scala/movies/part1/MovieOrder.scala
+++ src/main/scala/movies/part1/MovieOrder.scala
@@ -1,5 +1,15 @@
 package movies.part1
 
+import java.time.LocalDate
+
 object MovieOrder {
-  def orderingFrom(nameOrder: Order, releaseDateOrder: Order, rating: Order): Ordering[Movie] = ???
+  def orderingFrom(nameOrder: Order, releaseDateOrder: Order, rating: Order): Ordering[Movie] = {
+    new Ordering[Movie] {
+      def compare(m1: Movie, m2: Movie) = List(
+        nameOrder(Ordering[String]).compare(m1.name, m2.name),
+        releaseDateOrder(Ordering[LocalDate]).compare(m1.releaseDate, m2.releaseDate),
+        rating(Ordering[Int]).compare(m1.rating, m2.rating)
+      ).foldLeft(0)((acc, c) => if (acc != 0) acc else c)
+    }
+  }
 }
--- src/main/scala/movies/part1/Order.scala
+++ src/main/scala/movies/part1/Order.scala
@@ -2,11 +2,15 @@ package movies.part1
 
 sealed trait Order
 object Order {
-    case object Identity extends Order
-    case object Ascending extends Order
-    case object Descending extends Order
+  case object Identity extends Order
+  case object Ascending extends Order
+  case object Descending extends Order
 
-    implicit class Ops(val order: Order) extends AnyVal {
-        def apply[A](ordering: Ordering[A]): Ordering[A] = ???
+  implicit class Ops(val order: Order) extends AnyVal {
+    def apply[A](ordering: Ordering[A]): Ordering[A] = order match {
+      case Identity   => new Ordering[A] { def compare(x: A, y: A) = 0 }
+      case Ascending  => ordering
+      case Descending => ordering.reverse
     }
+  }
 }
